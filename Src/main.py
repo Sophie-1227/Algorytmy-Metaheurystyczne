a85a0c5b96585f83a13dd8010029f5fa401f966e
Bartek Grelewski <stilus43@gmail.com>
2022-03-14 19:22:44 +0100

    Rename of some files

--- Src/main.py
+++ Src/main.py
@@ -1,16 +1,11 @@
 import numpy as np
 import tsplib95
 
-
-
-
 if __name__ == '__main__':
     problem = tsplib95.load('../Data/bays29/bays29.tsp')
     opt = tsplib95.load('../Data/bays29/bays29.opt.tour')
 
-    # problem.tours = opt.tours
-    print(problem)
-    # print(opt.tours)
-    # print(problem.trace_tours(problem.tours))
-
-
+    problem.tours = opt.tours
+    #print(problem)
+    print(opt.tours)
+    print(problem.trace_tours(problem.tours))
