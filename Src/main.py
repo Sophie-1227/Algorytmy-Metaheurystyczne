543e2fd3cdde73c22482cb08951cf232f3f0a994
Bartek Grelewski <stilus43@gmail.com>
2022-03-16 17:02:37 +0100

    Functions

--- Src/main.py
+++ Src/main.py
@@ -1,17 +1,18 @@
 import numpy as np
 import tsplib95
 
+#zwraca sume wag
 def sum(problem):
      res = []
      for tour in problem.tours:
          cost = 0
-         for i in range(len(0, len(tour)-1)):
+         for i in range(0, len(tour)-1):
              cost += problem.get_weight(tour[i], tour[i+1])
          cost += problem.get_weight(tour[-1],tour[0])
          res.append(cost)
      return res
 
-
+#generuje wierzcholki
 def random_solve(problem):
     nodes = np.array(list(problem.get_nodes()))  # lista od 1 do ilosci wierzcholkow
     np.random.shuffle(nodes[1:-1])  # shufflowanie
@@ -21,13 +22,13 @@ def random_solve(problem):
 if __name__ == '__main__':
     #zapisuje caly obiekt
     problem = tsplib95.load('../Data/bays29/bays29.tsp')
-
-    s
-    #wypisanie macierzy
-    print(problem.edge_weights)
-
+    
+    
     #wypisywanie permutacji drogi
     random_solve(problem)
+
+    #wypisanie macierzy
+    #print(problem.edge_weights)
 
     #wypisywanie funkcji celu
     #ogarnac czy trace_tours zadziala
@@ -36,3 +37,4 @@ if __name__ == '__main__':
 
     #sum problem powinno dzialac ale nie wiem czy dziala xD
     #sum(problem)
+    
