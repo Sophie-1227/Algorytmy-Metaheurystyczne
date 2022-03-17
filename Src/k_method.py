49c97aa91d7ee8e586f5d6bce617ba6bf16602f4
Bartek Grelewski <stilus43@gmail.com>
2022-03-17 14:31:20 +0100

    Functions

--- Src/k_method.py
+++ Src/k_method.py
@@ -6,11 +6,13 @@ def k_method_solve():
 
     problem = tsplib95.load('../Data/bays29/bays29.tsp')
 
-    k = 1000
+    k = 10
     current = 8397420470283740084749
+    k_table = []
     for i in range (k):
         integer = random_solve(problem)
         if current > evaluate(problem):
             current = evaluate(problem)
             k_table = integer
+    print(current)
     print(k_table)
--- Src/main.py
+++ Src/main.py
@@ -19,7 +19,7 @@ def random_solve(problem):
     nodes = np.array(list(problem.get_nodes()))  # lista od 1 do ilosci wierzcholkow
     np.random.shuffle(nodes[1:-1])  # shufflowanie
     problem.tours.append(nodes.tolist())
-    print(problem.tours)
+    #print(problem.tours)
 
 if __name__ == '__main__':
     #zapisuje caly obiekt
