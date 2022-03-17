e9f1b9da7394018479ccc9d10f4e22d5751364b4
Sophie-1227 <demeter.1227.wroclaw@gmail.com>
2022-03-17 14:41:19 +0100

    Methods

--- Src/k_method.py
+++ Src/k_method.py
@@ -7,13 +7,12 @@ def k_method_solve():
     problem = tsplib95.load('../Data/bays29/bays29.tsp')
 
     #k - ilosc losowan (prob)
-    k = 10
+    k = 1000
     current = 8397420470283740084749
     for i in range (k):
         integer = random_solve(problem)
         if current > evaluate(problem):
             current = evaluate(problem)
             k_table = integer
-    print(current)
     print(k_table)
     # TODO ogarnac zapis do tablicy i wypisywanie
