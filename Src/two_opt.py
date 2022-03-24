b7c1cb9b91a7a974990dec4f9706d6d37d921e5a
Sophie-1227 <demeter.1227.wroclaw@gmail.com>
2022-03-24 14:26:47 +0100

    two_OPT second try

--- Src/two_opt.py
+++ Src/two_opt.py
@@ -5,10 +5,10 @@ def invert(array, i, j):
     length = j-i+1
     temp = []
 
-    for k in range (1,length):
+    for k in range (0,length):
        temp.append(array[k+i-1])
 
-    for k in range (1, length):
+    for k in range (0, length):
         array[i+k-1] = temp[length-k-1]
 
 
@@ -31,3 +31,5 @@ def two_opt(problem):
         if problem.trace_tours([curList])[0] == problem.trace_tours([tempList])[0]:
             return curList
         curList = tempList.copy()
+    print(curList)
+    print(problem.trace_tours([curList])[0])
