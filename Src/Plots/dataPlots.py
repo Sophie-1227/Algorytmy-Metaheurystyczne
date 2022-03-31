315ed7ae7eea41f0520b6aaa5f87f760b37d0f8a
Sophie-1227 <demeter.1227.wroclaw@gmail.com>
2022-03-31 11:04:42 +0200

    Small correction in plots initialization

--- Src/Plots/dataPlots.py
+++ Src/Plots/dataPlots.py
@@ -40,7 +40,3 @@ def twoOptTourPlot(problem):
     plt.plot([problem.get_display(c_list[i%len(c_list)])[0] for i in range(len(c_list)+1)],
              [problem.get_display(c_list[i%len(c_list)])[1] for i in range(len(c_list)+1)])
     plt.show()
-
-
-class NNAPlot:
-    pass
