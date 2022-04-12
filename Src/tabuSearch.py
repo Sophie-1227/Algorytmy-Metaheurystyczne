8a42c8d9dc95343397b6eef8e59b44740700b980
Bartek Grelewski <stilus43@gmail.com>
2022-04-12 22:22:21 +0200

    TSP ALGORYTHM -  Tabu Search first init

--- Src/tabuSearch.py
+++ Src/tabuSearch.py
@@ -3,6 +3,9 @@ import time
 
 import numpy as np
 
+from Src.Algorythms.NN_Algorythm import NNA
+
+
 def invert(array, i, j):
     length = j-i+1
     temp = []
@@ -40,6 +43,43 @@ def two_opt(problem, curList = None):
     while time.time() - time_start < 30:
     """
 
+def update(self, last_solution: np.array, last_cost: int):
+    last_solution = last_solution
+    last_cost = last_cost
+
+def basicSearch(neighboring_function, starting_solution: np.array):
+    time_start = time.time()
+    NNA_Path, NNA_Cost = NNA(problem, 0)
+    startingSolution = two_opt(problem, NNA_Path)[1]
+    #
+    bestSolution = startingSolution
+    bestCost = data.cost(startingSolution)
+
+    solution = bestSolution.copy()
+    tabooList = deque([], 20)
+    tabooList.append(solution)
+    while time.time() - time_start < 30:
+        neighboring_best_solution = np.array([])
+        neighboring_best_cost = np.inf
+        for neighboring_solution in neighboring_function(solution):
+            neighboring_cost = self.data.cost(neighboring_solution)
+            (taboo_list == neighboring_solution).any()
+            if neighboring_cost < neighboring_best_cost and (taboo_list == neighboring_solution).any():
+                neighboring_best_solution = neighboring_solution
+                neighboring_best_cost = neighboring_cost
+            solution = neighboring_best_solution
+            cost = neighboring_best_cost
+            taboo_list.append(solution)
+
+                if cost < best_cost:
+                    best_cost = cost
+                    best_solution = solution
+
+            self.__update(best_solution, best_cost)
+            return best_cost
+
+        def search(self, neighboring_function, starting_solution):
+            return self.__basic_search(neighboring_function=neighboring_function, starting_solution=starting_solution)
 
 
 
