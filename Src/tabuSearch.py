c12c68bd3f0edb426acc6f10774423c888b46ebd
Bartek Grelewski <stilus43@gmail.com>
2022-04-20 17:48:15 +0200

    Taboo Search basic version - working

--- Src/tabuSearch.py
+++ Src/tabuSearch.py
@@ -1,16 +1,14 @@
 from collections import deque
-from urllib.request import DataHandler
 
-import self as self
 import tsplib95
 import time
 
 import numpy as np
 
 from Src.Algorythms.NN_Algorythm import NNA
-from Src.utils import evaluate
 
-def invert_m(solution: list):
+
+def tabuInvert(solution: list):
     neighbours = []
     for i in range(len(solution)):
         for j in range(i + 1, len(solution)):
@@ -59,31 +57,25 @@ def get_cost(problem: tsplib95.models.StandardProblem, tour):
 
 class TabooSearch:
 
-    def __init__(self):
-        self.problem = tsplib95.load('../Data/bays29/bays29.tsp')
-
-
-
-    def __update(self, last_solution: np.array, last_cost: int):
-        self.last_solution = last_solution
-        self.last_cost = last_cost
-
     def basicSearch(self, neighboring_function, starting_solution: np.array,problem):
         time_start = time.time()
         NNA_Path, NNA_Cost = NNA(problem, 0)
         starting_solution, best_cost = two_opt(problem, NNA_Path)
         best_solution = starting_solution
 
+        def __update(self, last_solution: np.array, last_cost: int):
+            self.last_solution = last_solution
+            self.last_cost = last_cost
+
         solution = best_solution.copy()
-        taboo_list = deque([], 20) # <- struktura listy tabu - Kolejka?
+        lengthDeque = problem.dimension
+        taboo_list = deque([], lengthDeque) # <- struktura listy tabu deque
         taboo_list.append(solution)
-        while time.time() - time_start < 30: # <- Warunek stopu = czas
+        while time.time() - time_start < 5: # <- Warunek stopu = czas
             neighboring_best_solution = np.array([])
             neighboring_best_cost = np.inf
 
             for neighboring_solution in (n for n  in neighboring_function(solution) if n not in taboo_list):
-                # You can roughly think of any and all as series of logical or and and operators, respectively.
-                #if not any([x.all() for x in neighboring_solution == taboo_list]): # If neighboring solution is not in taboo list
                     if  get_cost(problem, neighboring_solution) < neighboring_best_cost:
                         neighboring_best_solution = neighboring_solution
                         neighboring_best_cost = get_cost(problem, neighboring_best_solution)
@@ -99,18 +91,11 @@ class TabooSearch:
         self.__update(best_solution, best_cost)
         return best_cost
 
-    pass
-
-    def search(self, neighboring_function, starting_solution):
-        return self.__basic_search(neighboring_function=neighboring_function, starting_solution=starting_solution)
-
-
-
 
 if __name__ == '__main__':
     problem = tsplib95.load('../Data/bays29/bays29.tsp')
-    #print (two_opt(problem))
     NNAPath, NNACost = NNA(problem, 0)
     startSolution, bestCost = two_opt(problem, NNAPath)
+    print(bestCost)
     taboo = TabooSearch()
-    print(taboo.basicSearch(neighboring_function = invert_m, starting_solution = startSolution, problem = problem))
+    print(taboo.basicSearch(neighboring_function = tabuInvert, starting_solution = startSolution, problem = problem))
