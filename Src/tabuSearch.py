bb8ff9727d7cd8c0b101f1e0fd4d671d84fdc927
Sophie-1227 <demeter.1227.wroclaw@gmail.com>
2022-04-20 13:19:35 +0200

    tabuSearch update

--- Src/tabuSearch.py
+++ Src/tabuSearch.py
@@ -1,5 +1,6 @@
 from collections import deque
 
+import self as self
 import tsplib95
 import time
 
@@ -9,14 +10,14 @@ from Src.Algorythms.NN_Algorythm import NNA
 
 
 def invert(array, i, j):
-    length = j-i+1
+    length = j - i + 1
     temp = []
 
-    for k in range (0,length):
-        temp.append(array[k+i-1])
+    for k in range(0, length):
+        temp.append(array[k + i - 1])
 
-    for k in range (0, length):
-        array[i+k-1] = temp[length-k-1]
+    for k in range(0, length):
+        array[i + k - 1] = temp[length - k - 1]
 
 def two_opt(problem, curList = None):
     if curList == None:
@@ -41,51 +42,50 @@ def two_opt(problem, curList = None):
     print(curList)
     print(problem.trace_tours([curList])[0])
 
-    """
-    while time.time() - time_start < 30:
-    """
+class TabooSearch:
 
-def update(self, lastSolution: np.array, lastCost: int):
-    lastSolution = lastSolution
-    lastCost = lastCost
+    def update(self, lastSolution: np.array, lastCost: int):
+        lastSolution = lastSolution
+        lastCost = lastCost
 
-def basicSearch(self, neighborFunction, startSolution: np.array):
-    startTime = time.time()
-    NNAPath, NNACost = NNA(problem, 0)
-    startSolution = two_opt(problem, NNAPath)[1]
+    def basicSearch(self, neighborFunction, startSolution: np.array):
+        startTime = time.time()
+        NNAPath, NNACost = NNA(problem, 0)
+        startSolution = two_opt(problem, NNAPath)[1]
 
-    bestSolution = startSolution
-    bestCost = problem.cost(startSolution)
+        bestSolution = startSolution
+        bestCost = problem.cost(startSolution)
 
-    solution = bestSolution.copy()
-    tabooList = deque([], 20)
-    tabooList.append(solution)
-    while time.time() - startTime < 30:
-        neighborBestSolution = np.array([])
-        neighborBestCost = np.inf
-        for neighborSolution in neighborFunction(solution):
-            neighborCost = self.data.cost(neighborSolution)
-            (tabooList == neighborSolution).any()
-            if neighborCost < neighborBestCost and (tabooList == neighborSolution).any():
-                neighborBestSolution = neighborSolution
-                neighborBestCost = neighborCost
-            solution = neighborBestSolution
-            cost = neighborBestCost
-            tabooList.append(solution)
+        solution = bestSolution.copy()
+        tabooList = deque([], 20)
+        tabooList.append(solution)
+        while time.time() - startTime < 30:
+            neighborBestSolution = np.array([])
+            neighborBestCost = np.inf
+            for neighborSolution in neighborFunction(solution):
+                neighborCost = self.data.cost(neighborSolution)
+                (tabooList == neighborSolution).any()
+                if neighborCost < neighborBestCost and (tabooList == neighborSolution).any():
+                    neighborBestSolution = neighborSolution
+                    neighborBestCost = neighborCost
+                solution = neighborBestSolution
+                cost = neighborBestCost
+                tabooList.append(solution)
 
-            if cost < bestCost:
-                bestCost = cost
-                bestSolution = solution
+                if cost < bestCost:
+                    bestCost = cost
+                    bestSolution = solution
 
-            self.__update(bestSolution, bestCost)
-            return bestCost
+                self.__update(bestSolution, bestCost)
+                return bestCost
 
-        def search(self, neighborFunction, startSolution):
-            return self.__basic_search(neighborFunction=neighborFunction, startSolution=startSolution)
+            def search(self, neighborFunction, startSolution):
+                return self.__basic_search(neighborFunction=neighborFunction, startSolution=startSolution)
 
 
 
 if __name__ == '__main__':
     problem = tsplib95.load('../Data/bays29/bays29.tsp')
     #print (two_opt(problem))
-    print(basicSearch(self, neighborFunction=invert, startSolution=None))
+    taboo = TabooSearch(self)
+    print(taboo.basicSearch(self, neighborFunction=None, startSolution=None))
