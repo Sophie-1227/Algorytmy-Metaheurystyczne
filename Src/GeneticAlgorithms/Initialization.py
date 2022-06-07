c70ed9e083728b7717df17c7d7ac2c02b4023f94
Sophie-1227 <demeter.1227.wroclaw@gmail.com>
2022-06-07 20:39:35 +0200

    Basic genetic algorithm and main function

--- Src/GeneticAlgorithms/Initialization.py
+++ Src/GeneticAlgorithms/Initialization.py
@@ -97,13 +97,13 @@ if __name__ == '__main__':
             print(k, min(fitnes_list), statistics.mean(fitnes_list))
             fitnes_list = getFitnes(mutated,problem)
             fitnes_list = sum(fitnes_list, [])
-            progenitor_list = selection(set, fitnes_list)
-            new_set = createPopulationSet(progenitor_list)
-            mutated = mutatePopulation(new_set)
 
         if min(fitnes_list) < best:
             best = min(fitnes_list)
-        # tworzy nowy set danych
 
+        # tworzy nowy set danych
+    progenitor_list = selection(set, fitnes_list)
+    new_set = createPopulationSet(progenitor_list)
+    mutated = mutatePopulation(new_set)
 
     print(best)
