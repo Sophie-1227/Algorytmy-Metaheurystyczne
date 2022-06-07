fc9eda8d7b933e02eb3f06eb710f571c275f4e9e
Bartek Grelewski <stilus43@gmail.com>
2022-06-07 22:21:04 +0200

    Changes by BEAST

--- Src/GeneticAlgorithms/Initialization.py
+++ Src/GeneticAlgorithms/Initialization.py
@@ -2,80 +2,104 @@ import numpy as np
 import tsplib95
 import statistics
 
-
 problem = tsplib95.load('/Users/grelewski/PycharmProjects/Metaheurystyka1/Data/bays29/bays29.tsp')
-mutation_rate = 0.3
+mutation_rate = 0.7
 dimension = problem.dimension
 populations_number = 100
+
 
 def firstPopulation(lista, populations_number):
     set = []
     for i in range(populations_number):
         lista = list(lista)
-        #lista = two_opt(problem)[0]
+        # lista = two_opt(problem)[0]
         np.random.shuffle(lista)
         set.append(lista)
-    return np.array(set)
+    return set
 
 
 def getFitnes(set, problem):
     fitnes_list = []
     for i in set:
-        c = list(i)
+        c = i[:]
         x = problem.trace_tours([c])
         fitnes_list.append(x)
     return fitnes_list
+
 
 def selection(set, fitnes_list):
     prob_list = []
 
     total_fitnes = sum(fitnes_list)
     for i in fitnes_list:
-        n = i/total_fitnes
+        n = i / total_fitnes
         prob_list.append(n)
 
-    a = np.random.choice(list(range(len(set))), len(set), p=prob_list, replace = True)
-    b = np.random.choice(list(range(len(set))), len(set), p=prob_list, replace = True)
+    a = np.random.choice(list(range(len(set))), len(set), p=prob_list, replace=True)
+    b = np.random.choice(list(range(len(set))), len(set), p=prob_list, replace=True)
 
-    a = set[a]
-    b = set[b]
+    off_a = []
+    for k in a:
+        off_a.append(set[k])
+    off_b = []
+    for k in b:
+        off_b.append(set[k])
 
-    return np.array([a,b])
+    return off_a, off_b
+
+
 # lista przodkow
 
 
 def createOffspring(p1, p2):
-    p1 = list(p1)
-    p2 = list(p2)
-    offspring = p1[0:5]
-    for node in p2:
-        if not node in offspring:
-            offspring.append(node)
-            #offspring = np.concatenate(offspring, [node])
+    piv1 = np.random.randint(0, len(p1))
+    piv2 = np.random.randint(0, len(p1))
+    if piv1 > piv2:
+        piv1, piv2 = piv2, piv1
+    offspring = [0 for _ in range(len(p1))]
+    used = [False for _ in range(len(p1))]
+    for i in range(piv1, piv2):
+        offspring[i] = p1[i]
+        used[p1[i]-1] = True
+    k = 0
+    for i in range(0, piv1):
+        while used[p2[k]-1]:
+            k += 1
+        offspring[i] = p2[k]
+        used[p2[k]-1] = True
+    for i in range(piv2, len(p1)):
+        while used[p2[k]-1]:
+            k += 1
+        offspring[i] = p2[k]
+        used[p2[k] - 1] = True
     return offspring
 
-def createPopulationSet(progenitor_list):
+
+def createPopulationSet(a, b):
     new_set = []
-    for i in range(progenitor_list.shape[1]):
-        p1, p2 = progenitor_list[0][i], progenitor_list[1][i]
-        offspring = createOffspring(p1, p2)
+    for i in range(len(a)):
+        offspring = createOffspring(a[i], b[i])
         new_set.append(offspring)
     return new_set
 
 
 
 def mutateOffspring(offspring):
-    for i in range(int(dimension*mutation_rate)):
+    for _ in range(np.random.randint(1, 10)):
         x = np.random.randint(0, dimension)
         y = np.random.randint(0, dimension)
 
         offspring[x], offspring[y] = offspring[y], offspring[x]
     return offspring
 
+
 def mutatePopulation(new_set):
     mutated = []
     for offspring in new_set:
-        mutated.append(mutateOffspring(offspring))
+        if np.random.random() < mutation_rate:
+            mutated.append(mutateOffspring(offspring))
+        else:
+            mutated.append(offspring)
     return mutated
 
 
